a548080ed63c903de1df2c93124809b5e980597a
Chris Wu <wakeup0501@gmail.com>
2022-06-23 22:56:55 +0800

    add update, delete, cancel function

--- client/src/services/course.service.js
+++ client/src/services/course.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const API_URL = "https://project-07.herokuapp.com/api/courses";
-const API_URL = "http://localhost:8080/api/courses";
+const API_URL = "https://project-07.herokuapp.com/api/courses";
+// const API_URL = "http://localhost:8080/api/courses";
 
 class CourseService {
     post(title, description, price) {
